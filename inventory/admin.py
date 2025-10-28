6422834c60d618f2f372a4c004525246fa61f227
SyedMahdi15 <syedmahdiaus1504@gmail.com>
2025-10-28 19:38:49 +1100

    Update admin.py

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from .models import Product, Inventory
-
+from .models.Cart import Cart, CartItem
+from .models.Order import Order, OrderItem
+from .models.Report import Report
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
@@ -12,3 +14,31 @@ class ProductAdmin(admin.ModelAdmin):
 class InventoryAdmin(admin.ModelAdmin):
     list_display = ('name',)
     filter_horizontal = ('products',)
+
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    list_display = ('customer', 'updated_at',)
+    search_fields = ('customer__name',)
+    list_filter = ('updated_at',)
+
+@admin.register(CartItem)
+class CartItemAdmin(admin.ModelAdmin):
+    list_display = ('cart', 'product', 'quantity', 'unit_price')
+    list_filter = ('product',)
+    search_fields = ('product__name',)
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'customer', 'created_at', 'total')
+    search_fields = ('id', 'customer__name')
+    date_hierarchy = 'created_at'
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ('order', 'product', 'quantity', 'unit_price')
+    search_fields = ('product__name',)
+
+@admin.register(Report)
+class ReportAdmin(admin.ModelAdmin):
+    list_display = ('name', 'created_at')
+    readonly_fields = ('data',)
